aaf61809d384892c2a5fda35db68a102b72bc067
Theguishe <guillermogacc0526@gmail.com>
2022-08-18 12:56:26 -0600

    CRUD - Contactos

--- src/Modelo/ModelContactos.java
+++ src/Modelo/ModelContactos.java
@@ -19,7 +19,7 @@ public class ModelContactos {
         
         try {
             connect = ModelConexion.getConnection();
-            String query = "SELECT * FROM tbContactos";
+            String query = "SELECT * FROM vwContactos";
             ps = connect.prepareStatement(query);
             rs = ps.executeQuery();
             return rs;
@@ -30,21 +30,17 @@ public class ModelContactos {
         
     }
     
-    public static ResultSet CargarCMBPersonal() {
-        
+    public static ResultSet CargarTablaPersonal() {
         Connection connect;
-        
         try {
             connect = ModelConexion.getConnection();
-            String query = "SELECT nombre_p, apellido_p FROM tbPersonal";
-            ps = connect.prepareStatement(query);
+            ps = connect.prepareStatement("SELECT * FROM vwPersonal_Contactos");
             rs = ps.executeQuery();
             return rs;
-        } catch(Exception e) {
-            JOptionPane.showMessageDialog(null, "Error al cargar los datos");
+        }catch (Exception e){
+            System.out.println("Error: " + e.toString());
             return null;
         }
-        
     }
     
     public static ResultSet CargarCMBTipoContacto() {
@@ -53,18 +49,18 @@ public class ModelContactos {
         
         try {
             connect = ModelConexion.getConnection();
-            String query = "SELECT * tbTipoContacto";
+            String query = "SELECT * FROM tbTipoContacto";
             ps = connect.prepareStatement(query);
             rs = ps.executeQuery();
             return rs;
         } catch(Exception e) {
-            JOptionPane.showMessageDialog(null, "Error al cargar los datos");
+            System.out.println("Error: " + e.toString());
             return null;
         }
         
     }
     
-    public static int RegistrarContacto(String contacto, int idpersonal, int idtipocontacto) {
+    public static boolean RegistrarContacto(String contacto, int idpersonal, int idtipocontacto) {
         
         Connection connect;
         
@@ -76,11 +72,37 @@ public class ModelContactos {
             ps.setInt(2, idpersonal);
             ps.setInt(3, idtipocontacto);
             ps.execute();
-            return 1;
+            return true;
         } catch(SQLException e) {
-            return 0;
+            return false;
         }
         
+    }
+    
+    public static ResultSet capturar_idPersonal(String contacto){
+        Connection connect;
+        try{
+            connect = ModelConexion.getConnection();
+            ps = connect.prepareStatement("SELECT idPersonal FROM tbContactos");
+            rs = ps.executeQuery();
+            return rs;
+        } catch(Exception e){
+            System.out.print("Error: " + e.toString());
+            return null;
+        }
+    }
+    
+    public static ResultSet cap_idPersonal_tbPersonal(String idper){
+        Connection connect;
+        try{
+            connect = ModelConexion.getConnection();
+            ps = connect.prepareStatement("SELECT idPersonal FROM tbPersonal");
+            rs = ps.executeQuery();
+            return rs;
+        } catch(Exception e){
+            System.out.print("Error: " + e.toString());
+            return null;
+        }
     }
     
     public static boolean ActualizarContacto(int idcontacto, String contacto, int idpersonal, int idtipocontacto) {
@@ -101,6 +123,19 @@ public class ModelContactos {
             return false;
         }
         
+    }
+    
+    public static ResultSet capturar_idContacto(){
+        Connection connect;
+        try{
+            connect = ModelConexion.getConnection();
+            ps = connect.prepareStatement("SELECT idContacto FROM tbContactos");
+            rs = ps.executeQuery();
+            return rs;
+        } catch(Exception e){
+            System.out.print("Error: " + e.toString());
+            return null;
+        }
     }
     
     public static boolean EliminarContacto(int idcontacto) {
